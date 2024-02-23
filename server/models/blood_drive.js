aaebf1727a57218ac65143e79dd20a821c219324
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-23 12:36:25 +0100

    fix: blood drive userId fix

--- server/models/blood_drive.js
+++ server/models/blood_drive.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const User = require("./user");
 const { Schema } = mongoose;
 
 const DriveSchema = new Schema(
   {
     title: { type: "String", required: true },
     tokenAmount: { type: "String", required: true },
-    userId: { type: "String", required: true },
+    userId: { type: mongoose.Types.ObjectId, ref:User, required: true },
     startDate: { type: "String", required: true },
     endDate: { type:"String", required: true }
   },

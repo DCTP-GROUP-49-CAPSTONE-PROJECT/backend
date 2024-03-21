bcd8224bf537d80464ab6f8ea8a1a5934bf40467
Adetunji Adebowale <bowaleadetunji@gmail.com>
2024-03-21 12:42:55 +0100

    update

--- server/controllers/discussion.js
+++ server/controllers/discussion.js
@@ -4,8 +4,60 @@ const router = express.Router();
 const discussion = require("../services/discussion");
 
 router.post("/create-discussion", async (req, res) => {
+  const { title, posted_by, content } = req.body;
+
+  if (!title || !posted_by || !content) {
+    res.status(400).json({
+      error: "one or more field is not filled",
+    });
+  } else {
+    const data = {
+      title: title,
+      posted_by: posted_by,
+      content: content,
+    };
+    const newDiscussion = await discussion.createDiscussion(data);
+    if (newDiscussion) {
+      res.status(201).json(newDiscussion);
+    } else {
+      res.status(400).json(newDiscussion);
+    }
+  }
+});
+
+router.get("/all-discussion", async (req, res, next) => {
+  try {
+    const discussions = await discussion.getAllDiscussion();
+    res.status(201).json(discussions);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/single-discussion", async (req, res, next) => {
+  try {
+    const id = req.query.id;
+    const getdiscussion = await discussion.getDiscussionById(id);
+    res.status(201).json(getdiscussion);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/discussions-by-user", async (req, res, next) => {
+  try {
+    const id = req.query.user_id;
+    const getdiscussions = await discussion.getDiscussionByUserId(id);
+    res.status(201).json(getdiscussions);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/update-discussion", async (req, res) => {
+  try {
+    const { discussion_id } = req.query;
     const { title, posted_by, content } = req.body;
-  
     if (!title || !posted_by || !content) {
       res.status(400).json({
         error: "one or more field is not filled",
@@ -14,60 +66,8 @@ router.post("/create-discussion", async (req, res) => {
       const data = {
         title: title,
         posted_by: posted_by,
-        content: content
-      };
-      const newDiscussion = await discussion.createDiscussion(data);
-      if (newDiscussion) {
-        res.status(201).json(newDiscussion);
-      } else {
-        res.status(400).json(newDiscussion);
-      }
-    }
-});
-
-router.get("/all-discussion", async (req, res, next) => {
-  try {
-    const discussions = await discussion.getAllDiscussion();
-    res.status(201).json(discussions);
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-router.get("/single-discussion", async (req,res,next) => {
-  try {
-    const id = req.query.id
-    const getdiscussion = await discussion.getDiscussionById(id);
-    res.status(201).json(getdiscussion);
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-router.get("/discussions-by-user", async (req,res,next) => {
-  try {
-    const id = req.query.user_id
-    const getdiscussions = await discussion.getDiscussionByUserId(id);
-    res.status(201).json(getdiscussions);
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-router.put("/update-discussion", async (req,res) => {
-  try {
-    const { discussion_id } = req.query
-    const { title, posted_by, content } = req.body;
-    if (!title || !posted_by || !content) {
-      res.status(400).json({
-        error: "one or more field is not filled",
-      });
-    }else{
-      const data = {
-        title: title,
-        posted_by: posted_by,
         content: content,
-        discussion_id: discussion_id
+        discussion_id: discussion_id,
       };
       const updateDiscussion = await discussion.updateDiscussionDetails(data);
       if (updateDiscussion) {
@@ -77,18 +77,18 @@ router.put("/update-discussion", async (req,res) => {
       }
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 router.delete("/delete-discussion", async (req, res) => {
   try {
-    const { discussion_id } = req.query
+    const { discussion_id } = req.query;
     const discussions = await discussion.deleteDiscussion(discussion_id);
     res.status(204).json(discussions);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-  module.exports = router;
+module.exports = router;
--- server/services/comment.js
+++ server/services/comment.js
@@ -1,45 +1,50 @@
-const Comment = require('../models/comment')
+const Comment = require("../models/comment");
 
 // creates a new Comment
-const createComment = async ({discussion_id, commented_by, comment_to_add}) => {
-    try {
-        const addcomment = new Comment({
-            discussion_id, 
-            commented_by, 
-            comment:comment_to_add
-          });
-          if (await addcomment.save()) {
-            return [true, addcomment];
-          }
-    } catch (error) {
-        console.log(error)
+const createComment = async ({
+  discussion_id,
+  commented_by,
+  comment_to_add,
+}) => {
+  try {
+    const addcomment = new Comment({
+      discussion_id,
+      commented_by,
+      comment: comment_to_add,
+    });
+    if (await addcomment.save()) {
+      return [true, addcomment];
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 /* Return Comment with specified id */
 const getCommentById = async (id) => {
-    const comment = await Comment.findById({_id: id});
-    return comment;
+  const comment = await Comment.findById({ _id: id });
+  return comment;
 };
 
 /* Return Comment with specified id */
 const getCommentByUserId = async (id) => {
-  const comments = await Comment.find({commented_by:id})
+  const comments = await Comment.find({ commented_by: id });
   return comments;
 };
-  
+
 /* Return all Comment */
 const getAllComment = async () => {
-    return await Comment.find();
+  return await Comment.find();
 };
 
 const getAllCommentsByDiscussion = async (discussionID) => {
-    return await Comment.find({discussion_id: discussionID});
+  return await Comment.find({ discussion_id: discussionID });
 };
 
 module.exports = {
-    createComment,
-    getCommentById,
-    getCommentByUserId,
-    getAllComment,
-  };
+  createComment,
+  getCommentById,
+  getCommentByUserId,
+  getAllComment,
+  getAllCommentsByDiscussion,
+};
